8956dfc16727c895be0c4d7752d9ec5b73c10aca
lmfaber <l-faber@posteo.de>
2025-02-13 13:56:30 +0100

    Update plugin information

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,44 @@
 <?php
 
-namespace Kanboard\Plugin\AutomaticAction;
+namespace Kanboard\Plugin\TaskMoveColumnNotMovedPeriodByPriority;
 
 use Kanboard\Core\Plugin\Base;
-use Kanboard\Plugin\AutomaticAction\Action\TaskMoveColumnNotMovedPeriodByPriority;
+use Kanboard\Plugin\TaskMoveColumnNotMovedPeriodByPriority\Action\TaskMoveColumnNotMovedPeriodByPriority;
 
 class Plugin extends Base
 {
     public function initialize()
     {
         $this->actionManager->register(new TaskMoveColumnNotMovedPeriodByPriority($this->container));
+    }
+
+        public function getPluginName()
+    {
+        return 'TaskMoveColumnNotMovedPeriodByPriority';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Move the task to another column when not moved during a given period based on priority');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Lasse Faber';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/DKFZ-NGSCF/kanboard-TaskMoveColumnNotMovedPeriodByPriority';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.2.43';
     }
 }
